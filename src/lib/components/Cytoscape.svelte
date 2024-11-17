8d81466c3fb1524f1eb10851b0747a773a2239f9
James Ding <jamesding365@gmail.com>
2024-11-17 10:51:41 -0600

    Finalize web

--- src/lib/components/Cytoscape.svelte
+++ src/lib/components/Cytoscape.svelte
@@ -1,17 +1,131 @@
 <script lang="ts">
     import {onMount} from "svelte";
-    import cytoscape from "cytoscape";
+    import cytoscape, {type Stylesheet} from "cytoscape";
     import cytoscapeFcose from "cytoscape-fcose"
+    import tippy from "tippy.js";
+    import cytoscapePopper from "cytoscape-popper";
 
-    export let courseData: cytoscape.ElementDefinition[]
-    export let cytoscapeStyles: cytoscape.Stylesheet[]
+    export let url: string
+    export let styleUrl: string
 
     let cy: cytoscape.Core;
 
-    onMount(() => {
+    function tippyFactory(ref: any, content: any) {
+        // Since tippy constructor requires DOM element/elements, create a placeholder
+        const dummyDomEle = document.createElement('div');
 
-        console.log(courseData)
+        return tippy(dummyDomEle, {
+            getReferenceClientRect: ref.getBoundingClientRect,
+            trigger: 'manual', // mandatory
+            content: content,
+            arrow: true,
+            placement: 'bottom',
+            hideOnClick: true,
+            sticky: "reference",
+            interactive: true,
+            appendTo: document.body // or append dummyDomEle to document.body
+        })
+    }
+
+    let myTip: any;
+
+    function setTip(newTip: any) {
+        myTip?.destroy();
+        myTip = newTip;
+    }
+
+
+    onMount(async () => {
+
+        let response = await fetch(url);
+        let courseData = await response.json();
+
+        let styleResponse = await fetch(styleUrl);
+        let styleData = await styleResponse.json();
+
+        let cytoscapeStyles : Stylesheet[] = [
+            {
+                selector: 'node',
+                style: {
+                    'label': (data: { data: (key: string) => any, id: () => string }): string => {
+                        return data.data('label') || data.id();
+                    },
+                    'text-valign': 'center',
+                    'text-halign': 'center',
+                    'background-color': '#757575',
+                }
+            },
+            {
+                selector: '.highlighted-nodes',
+                style: {
+                    'border-width': 1,
+                    'border-color': '#000',
+                    'border-style': 'solid',
+                }
+            },
+            {
+                selector: '.faded',
+                style: {
+                    'opacity': 0.25,
+                    'text-opacity': 0.25,
+                }
+            },
+            {
+                selector: '*',
+                style: {
+                    'transition-property': 'opacity',
+                    'transition-duration': 0.2,
+                }
+            },
+            {
+                selector: 'node[type="compound"]',
+                style: {
+                    'text-valign': 'top',
+                    'border-width': 0,
+                    'background-opacity': 0,
+                    label: '',
+                }
+            },
+            {
+                selector: 'edge',
+                style: {
+                    'width': 1,
+                    'line-color': '#000',
+                    'curve-style': 'straight',
+                    'target-arrow-color': '#000',
+                    'target-arrow-shape': 'triangle',
+                    'source-distance-from-node': 5,
+                    'target-distance-from-node': 5,
+                    'text-wrap': 'wrap',
+                    'font-size': 10,
+                }
+            },
+            {
+                selector: '.highlighted-edges',
+                    style: {
+                        'width': 2,
+                    }
+            }
+        ]
+
+        const styles = styleData.map(item => {
+            const [parent, color] = Object.entries(item)[0];
+            return {
+                selector: `node[parent="${parent}"]`,
+                style: {
+                    'background-color': color,
+                },
+            };
+        });
+
+        cytoscapeStyles = cytoscapeStyles.concat(styles);
+
+
+        console.log(cytoscapeStyles)
+
+
         cytoscape.use(cytoscapeFcose);
+        cytoscape.use(cytoscapePopper(tippyFactory))
 
         let cytoscapeLayout: cytoscape.BreadthFirstLayoutOptions = {
             name: 'breadthfirst',
@@ -74,6 +188,74 @@
         }
 
         cy.layout(newCytoscapeLayout).run();
+
+        cy.on('mouseover', 'node', function (event) {
+            const targetNode = event.target;
+            highlightPath(targetNode);
+        });
+
+        cy.on('mouseout', 'node', function (event) {
+            cy.nodes().removeClass('highlighted-nodes');
+            cy.elements().removeClass('faded');
+            cy.edges().removeClass('highlighted-edges');
+            myTip?.destroy();
+        });
+
+        cy.on('click', 'node', async function (event) {
+            const targetNode = event.target;
+            if (targetNode?.data('type') === 'compound') {
+                return;
+            }
+
+            let tip = targetNode.popper({
+                content: () => {
+                    let div = document.createElement('div');
+                    div.innerHTML = `
+                        <div class="bg-black text-white p-2 rounded-lg">
+                            <h1 class="text-lg font-semibold">${targetNode.id()}</h1>
+                            <p class="text-sm">${targetNode.data('description')}</p>
+                        </div>
+                    `;
+                    return div;
+                },
+            });
+            tip.show();
+
+            setTip(tip);
+        });
+
+        function getNonCompoundNodes() {
+            return cy.nodes().filter(function (node) {
+                return node.data('type') !== 'compound';
+            });
+        }
+
+        function highlightPath(node: cytoscape.NodeSingular) {
+
+            if (node.data('type') === 'compound') {
+                return;
+            }
+
+            const incomingNodes = node.predecessors('node').union(node);
+            const incomingEdges = node.predecessors('edge');
+
+            const outgoingNodes = node.outgoers('node').union(node);
+            const outgoingEdges = node.outgoers('edge');
+
+            const highlightedNodes = incomingNodes.union(outgoingNodes);
+            const highlightedEdges = incomingEdges.union(outgoingEdges);
+
+            highlightedNodes.addClass('highlighted-nodes');
+            highlightedEdges.addClass('highlighted-edges');
+
+            const nonCompoundNodes = getNonCompoundNodes();
+
+            const fadeNodes = nonCompoundNodes.difference(highlightedNodes);
+            const fadeEdges = cy.edges().difference(highlightedEdges);
+            fadeNodes.addClass('faded');
+            fadeEdges.addClass('faded');
+
+        }
 
     })
 
