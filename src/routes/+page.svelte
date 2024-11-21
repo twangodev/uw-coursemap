358caeda16f14c96085e67e8f43c9253822e2832
James Ding <jamesding365@gmail.com>
2024-11-21 09:37:45 -0600

    Update routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,4 @@
 </script>
 
 <ContentWrapper>
-    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 </ContentWrapper>
---
+++ src/routes/explorer/all/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+
+    import Cytoscape from "$lib/components/Cytoscape.svelte";
+    import {PUBLIC_API_URL} from "$env/static/public";
+
+</script>
+
+<Cytoscape url="{PUBLIC_API_URL}/global_graph.json" styleUrl="{PUBLIC_API_URL}/global_style.json"/>
