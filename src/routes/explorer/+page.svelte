6d9eab07d022abfb589bd56206aac26ee97a093c
James Ding <jamesding365@gmail.com>
2024-11-22 18:39:41 -0600

    Implement graph all button

--- src/routes/explorer/+page.svelte
+++ src/routes/explorer/+page.svelte
@@ -4,6 +4,8 @@
     import {PageHeader, PageHeaderDescription, PageHeaderHeading} from "$lib/components/page-header";
     import {Button} from "$lib/components/ui/button";
     import Search from "$lib/components/Search.svelte";
+    import * as AlertDialog from "$lib/components/ui/alert-dialog/index.js";
+    import {goto} from "$app/navigation";
 
 </script>
 
@@ -18,7 +20,23 @@
         </p>
         <div class="flex w-full items-center justify-center space-x-4 py-4 md:pb-10">
             <Search wide={true}/>
-            <Button variant="outline">Graph All</Button>
+            <AlertDialog.Root>
+                <AlertDialog.Trigger asChild let:builder>
+                    <Button builders={[builder]} variant="outline">Graph All</Button>
+                </AlertDialog.Trigger>
+                <AlertDialog.Content>
+                    <AlertDialog.Header>
+                        <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+                        <AlertDialog.Description>
+                            Graphing all courses and programs may take a long time and may cause your browser to become unresponsive.
+                        </AlertDialog.Description>
+                    </AlertDialog.Header>
+                    <AlertDialog.Footer>
+                        <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+                        <AlertDialog.Action on:click={() => goto('/explorer/all')}>Continue</AlertDialog.Action>
+                    </AlertDialog.Footer>
+                </AlertDialog.Content>
+            </AlertDialog.Root>
         </div>
     </PageHeader>
 </ContentWrapper>
