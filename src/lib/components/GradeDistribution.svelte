66d7337da5ef23865d91ae23f7b91702c86381e0
James Ding <jamesding365@gmail.com>
2024-11-28 17:33:58 -0600

    Add simple grade distribution chart

---
+++ src/lib/components/GradeDistribution.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+
+    import type {MadgradesData} from "$lib/types/madgrades.ts";
+    import {BarChartSimple, ScaleTypes} from '@carbon/charts-svelte'
+    import '@carbon/charts-svelte/styles.css'
+
+    export let madgradesData: MadgradesData;
+    export let term: string | null = null;
+
+    let gradeData = term ? madgradesData.by_term[term] : madgradesData.cumulative
+    let data = [
+        {
+            group: 'A',
+            value: gradeData.a
+        },
+        {
+            group: 'AB',
+            value: gradeData.ab
+        },
+        {
+            group: 'B',
+            value: gradeData.b
+        },
+        {
+            group: 'BC',
+            value: gradeData.bc
+        },
+        {
+            group: 'C',
+            value: gradeData.c
+        },
+        {
+            group: 'D',
+            value: gradeData.d
+        },
+        {
+            group: 'F',
+            value: gradeData.f
+        }
+    ].reverse()
+
+    let options = {
+        title: 'Grade Distribution',
+        axes: {
+            left: {
+                mapsTo: 'group',
+                scaleType: ScaleTypes.LABELS,
+            },
+            bottom: {
+                mapsTo: 'value'
+            }
+        },
+        legend: {
+            order: [
+                'A',
+                'AB',
+                'B',
+                'BC',
+                'C',
+                'D',
+                'F'
+            ]
+        },
+        toolbar: {
+            enabled: true
+        }
+    }
+
+</script>
+
+<div class="h-full">
+    <BarChartSimple {data} {options}/>
+</div>
