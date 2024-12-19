22a1aef117a345ad3950315d37aef43b75b5a311
James Ding <jamesding365@gmail.com>
2024-12-19 13:32:22 -0600

    Add GradeDataStackedAreaChart.svelte

---
+++ src/lib/components/charts/GradeDataStackedAreaChart.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import {type ChartTabularData, StackedAreaChart, type StackedAreaChartOptions} from '@carbon/charts-svelte';
+
+    let data: ChartTabularData = [
+        {group: 'A', value: 1},
+        {group: 'B', value: 2},
+        {group: 'C', value: 3},
+        {group: 'D', value: 4},
+        {group: 'E', value: 5},
+        {group: 'F', value: 6},
+    ]
+
+    let options: StackedAreaChartOptions = {
+
+    }
+
+
+</script>
+
+<s></s>
+<StackedAreaChart {data} {options}/>
