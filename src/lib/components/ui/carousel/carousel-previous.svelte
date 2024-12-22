5705daf91b807c60f71aa46601cb95639d6b3569
James Ding <jamesding365@gmail.com>
2024-12-22 10:36:11 -0800

    Update carousel-previous.svelte

--- src/lib/components/ui/carousel/carousel-previous.svelte
+++ src/lib/components/ui/carousel/carousel-previous.svelte
@@ -20,21 +20,25 @@
 		getEmblaContext("<Carousel.Previous/>");
 </script>
 
-<Button
-	{variant}
-	{size}
-	class={cn(
+<div class={cn(
+		"transition-opacity", $canScrollPrev ? "opacity-100" : "opacity-0" )} >
+	<Button
+			{variant}
+			{size}
+			class={cn(
 		"absolute h-8 w-8 touch-manipulation rounded-full",
 		$orientation === "horizontal"
-			? "-left-12 top-1/2 -translate-y-1/2"
+			? "left-2 top-1/2 -translate-y-1/2"
 			: "-top-12 left-1/2 -translate-x-1/2 rotate-90",
 		className
 	)}
-	disabled={!$canScrollPrev}
-	on:click={scrollPrev}
-	on:keydown={handleKeyDown}
-	{...$$restProps}
->
-	<ArrowLeft class="h-4 w-4" />
-	<span class="sr-only">Previous slide</span>
-</Button>
+			disabled={!$canScrollPrev}
+			on:click={scrollPrev}
+			on:keydown={handleKeyDown}
+			{...$$restProps}
+	>
+		<ArrowLeft class="h-4 w-4" />
+		<span class="sr-only">Previous slide</span>
+	</Button>
+
+</div>
