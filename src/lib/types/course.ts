bb9deb5a46ad50fa4179bc0ef51be1456f6949e1
James Ding <jamesding365@gmail.com>
2024-12-22 10:35:34 -0800

    Add null fields

--- src/lib/types/course.ts
+++ src/lib/types/course.ts
@@ -1,7 +1,8 @@
 import type {MadgradesData} from "$lib/types/madgrades.ts";
 import type {EnrollmentData} from "$lib/types/enrollment.ts";
+import {PUBLIC_API_URL} from "$env/static/public";
 
-type CourseReference = {
+export type CourseReference = {
     subjects: string[];
     course_number: number;
 }
@@ -17,11 +18,25 @@ export type Course = {
     description: string
     prerequisites: CoursePrerequisites,
     optimized_prerequisites: CoursePrerequisites,
-    madgrades_data: MadgradesData,
-    enrollment_data: EnrollmentData,
+    madgrades_data: MadgradesData | null,
+    enrollment_data: EnrollmentData | null,
+}
+
+export async function courseReferenceStringToCourse(sanatizedCourseReferenceString: string): Promise<Course> {
+    let response = await fetch(`${PUBLIC_API_URL}/course/${sanatizedCourseReferenceString}.json`)
+    return await response.json();
 }
 
 export function courseReferenceToString(courseReference: CourseReference): string {
     let subjects = courseReference.subjects.sort().join("/");
     return `${subjects} ${courseReference.course_number}`;
+}
+
+export function sanitizeCourseToReferenceString(courseReference: CourseReference): string {
+    return courseReferenceToString(courseReference).replaceAll(" ", "_").replaceAll("/", "_");
+}
+
+export async function courseReferenceToCourse(courseReference: CourseReference): Promise<Course> {
+    let sanitizedCourseReferenceToString = sanitizeCourseToReferenceString(courseReference);
+    return await courseReferenceStringToCourse(sanitizedCourseReferenceToString);
 }
