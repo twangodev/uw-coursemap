c52869c977cf010c87b0cefca9733dddfd1ddbba
James Ding <jamesding365@gmail.com>
2024-12-22 10:34:48 -0800

    Add course carousel

---
+++ src/lib/components/course-carousel/CourseCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+    import {type Course, courseReferenceToString, sanitizeCourseToReferenceString} from "$lib/types/course.ts";
+    import {Card, CardDescription, CardHeader, CardTitle} from "$lib/components/ui/card/index.js";
+
+    export let course: Course
+
+</script>
+
+<a href="/course/{sanitizeCourseToReferenceString(course.course_reference)}" target="_parent"> <!-- TODO fix reactivity on +page.svelte for courseIdentifier slug -->
+    <Card class="w-full max-w-64 overflow-hidden">
+        <CardHeader class="pb-0">
+            <CardTitle class="truncate">{course.course_title}</CardTitle>
+            <CardDescription class="font-bold truncate">{courseReferenceToString(course.course_reference)}</CardDescription>
+            <CardDescription class="line-clamp-4">{course.description}</CardDescription>
+        </CardHeader>
+    </Card>
+</a>
---
+++ src/lib/components/course-carousel/CourseCarousel.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+    import * as Carousel from "$lib/components/ui/carousel/index.js";
+    import {
+        type Course,
+        type CourseReference,
+        courseReferenceToCourse
+    } from "$lib/types/course.ts";
+    import CourseCard from "$lib/components/course-carousel/CourseCard.svelte";
+    import {onMount} from "svelte";
+    import CourseCardSkeleton from "$lib/components/course-carousel/CourseCardSkeleton.svelte";
+
+    export let courseReferences: CourseReference[];
+
+    let courses: Course[]; // Array to hold fetched Course objects
+    let loadedCourses = false;
+
+    onMount(async () => {
+        loadedCourses = false;
+        courses = await Promise.all(
+            courseReferences.map(async (courseReference) => {
+                return await courseReferenceToCourse(courseReference);
+            })
+        );
+        loadedCourses = true;
+    });
+</script>
+{#if courseReferences.length === 0}
+    <p>No courses found</p>
+{:else}
+    {#if !loadedCourses}
+        <Carousel.Root>
+            <Carousel.Content>
+                {#each courseReferences as _}
+                    <Carousel.Item class="basis-1/2 md:basis-1/3 lg:basis-1/4">
+                            <CourseCardSkeleton />
+                    </Carousel.Item>
+                {/each}
+            </Carousel.Content>
+        </Carousel.Root>
+    {:else}
+        <Carousel.Root>
+            <Carousel.Content>
+                {#each courses as course}
+                    <Carousel.Item class="basis-1/2 md:basis-1/3 lg:basis-1/4">
+                        <CourseCard {course} />
+                    </Carousel.Item>
+                {/each}
+            </Carousel.Content>
+            <Carousel.Previous />
+            <Carousel.Next />
+        </Carousel.Root>
+    {/if}
+{/if}
