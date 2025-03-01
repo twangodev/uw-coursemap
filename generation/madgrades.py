d7e741d1e4ab8680e0bc5aefa59ec30a9fbff6e7
James Ding <jamesding365@gmail.com>
2025-03-01 17:02:55 -0600

    Remove large code comments

--- generation/madgrades.py
+++ generation/madgrades.py
@@ -20,63 +20,6 @@ def get_madgrades_terms(madgrades_api_key, logger: Logger) -> dict[int, str]:
     response = requests.get(url=madgrades_api_endpoint + "terms", headers=auth_header)
     return { int(term_code): term_name for term_code, term_name in response.json().items() }
 
-# def build_from_pagination(course_ref_to_course, url, madgrades_api_key, logger: Logger, session: Session = None):
-#     if url is None:
-#         return
-#
-#     time_start = time.time()
-#     logger.debug(f"Fetching madgrades data from {url}")
-#
-#     if session is None:
-#         prefix = get_prefix(url)
-#         session = requests.Session()
-#         adapter = HTTPAdapter(pool_connections=10, pool_maxsize=10, max_retries=get_global_retry_strategy())
-#         session.mount(prefix, adapter)
-#
-#     auth_header = {"Authorization": f"Token token={madgrades_api_key}" }
-#
-#     response = requests.get(url=url, headers=auth_header)
-#     data = response.json()
-#
-#     current_page = data["currentPage"]
-#     total_pages = data["totalPages"]
-#     results = data["results"]
-#
-#     time_elapsed_ms = get_ms(time_start)
-#     logger.info(f"Discovered {len(results)} courses in {time_elapsed_ms} ({current_page}/{total_pages} pages)")
-#
-#     i = 0
-#     for madgrade_course in results:
-#         i += 1
-#         time_start = time.time()
-#         course_number = madgrade_course["number"]
-#         subjects = madgrade_course["subjects"]
-#
-#         subject_set = set()
-#         for subject in subjects:
-#             abbreviation = subject["abbreviation"]
-#             parsed_abbr = abbreviation.replace(" ", "")
-#             subject_set.add(parsed_abbr)
-#
-#         course_ref = Course.Reference(subject_set, course_number)
-#
-#         if course_ref not in course_ref_to_course:
-#             logger.debug(f"Unknown course discovered from Madgrades: {course_ref}")
-#             continue
-#
-#         grades_url = madgrade_course["url"] + "/grades"
-#         madgrades_data = MadgradesData.from_madgrades(grades_url, madgrades_api_key)
-#
-#         course = course_ref_to_course[course_ref]
-#         course.madgrades_data = madgrades_data
-#
-#         time_elapsed_ms = get_ms(time_start)
-#         logger.info(f"Adding madgrades data to course {course_ref} in {time_elapsed_ms} ({i}/{len(results)}) of page {current_page}/{total_pages}")
-#
-#
-#     next_page_url = data["nextPageUrl"]
-#     build_from_pagination(course_ref_to_course, next_page_url, madgrades_api_key, logger, session)
-
 async def process_course(session, madgrade_course, course_ref_to_course, madgrades_api_key, logger, current_page, total_pages):
     course_number = madgrade_course["number"]
     subjects = madgrade_course["subjects"]
