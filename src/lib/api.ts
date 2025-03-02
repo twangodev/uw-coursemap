77b3dbea9c476fac343aa15de0802ae842cb1331
James Ding <jamesding365@gmail.com>
2025-03-02 17:19:52 -0600

    Fix PUBLIC_SEARCH_API_URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,11 +1,11 @@
-import {PUBLIC_API_SEARCH_URL, PUBLIC_API_URL} from "$env/static/public";
+import {PUBLIC_API_URL, PUBLIC_SEARCH_API_URL} from "$env/static/public";
 
 export async function apiFetch(path: string): Promise<Response> {
     return await fetch(`${PUBLIC_API_URL}${path}`);
 }
 
 export async function apiQueryFetch(query: string): Promise<Response> {
-    return await fetch(`${PUBLIC_API_SEARCH_URL}/query`, {
+    return await fetch(`${PUBLIC_SEARCH_API_URL}/query`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
