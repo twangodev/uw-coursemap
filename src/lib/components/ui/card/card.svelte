a5f251b7258e2ab94b4c94c3901d5ff5678025d3
James Ding <jamesding365@gmail.com>
2025-03-03 01:11:17 -0600

    Revert cards

--- src/lib/components/ui/card/card.svelte
+++ src/lib/components/ui/card/card.svelte
@@ -13,7 +13,7 @@
 
 <div
 	bind:this={ref}
-	class={cn("bg-card text-card-foreground rounded-lg border shadow-sm", className)}
+	class={cn("bg-card text-card-foreground rounded-lg border shadow-sm pb-6", className)}
 	{...restProps}
 >
 	{@render children?.()}
