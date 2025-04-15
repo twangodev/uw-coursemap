3b918968271d802197a55c924175f7936ef78cca
ProfessorAtomicManiac <59379639+ProfessorAtomicManiac@users.noreply.github.com>
2025-04-14 21:51:43 -0500

    added custom alert-close

--- src/lib/components/ui/alert/index.ts
+++ src/lib/components/ui/alert/index.ts
@@ -3,6 +3,7 @@ import { type VariantProps, tv } from "tailwind-variants";
 import Root from "./alert.svelte";
 import Description from "./alert-description.svelte";
 import Title from "./alert-title.svelte";
+import Close from "./alert-close.svelte";
 
 export const alertVariants = tv({
 	base: "[&>svg]:text-foreground relative w-full rounded-lg border p-4 [&:has(svg)]:pl-11 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4",
@@ -30,4 +31,6 @@ export {
 	Root as Alert,
 	Description as AlertDescription,
 	Title as AlertTitle,
+	// Close is made by us, not shadcn/ui
+	Close as AlertClose,
 };
