3e2ffabed63a873d99a35cadb012cbfe711c1121
TheRad1 <landon.bakken@gmail.com>
2025-04-17 12:12:10 -0500

    added hyphen

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -69,7 +69,8 @@
             }
             
             //get course subject and numbers
-            const regex = /(\b[A-Z&]+(?:\s[A-Z&]+)*\b)\s+(X\d{2}|\d{3})/g;
+            //make sure to keep "-" at the start of the subject search otherwise it will be seen as a range
+            const regex = /(\b[-A-Z&]+(?:\s[-A-Z&]+)*\b)\s+(X\d{2}|\d{3})/g;
             let matches, results = [];
             
             while ((matches = regex.exec(text)) !== null) {
