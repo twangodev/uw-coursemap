c05ffcbbbb6df873931d54fb6ed7a63729cfed9a
James Ding <jamesding365@gmail.com>
2025-04-20 01:23:40 -0500

    Finalize documentation template

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,31 +1,110 @@
-import { defineConfig } from 'vitepress'
-import {withSidebar} from "vitepress-sidebar";
-import type {VitePressSidebarOptions} from "vitepress-sidebar/types";
+import {defineConfig, type UserConfig} from 'vitepress'
 
-const vitePressSidebarOptions: VitePressSidebarOptions  = {
-  documentRootPath: '/docs',
-  collapsed: false,
-  capitalizeFirst: true,
-  capitalizeEachWords: true,
-  hyphenToSpace: true,
-  useTitleFromFileHeading: true,
-  useFolderTitleFromIndexFile: true,
-};
-
-const vitePressOptions = {
+const vitePressOptions: UserConfig = {
+  lang: 'en-US',
   title: "uw-coursemap",
   description: "Explore the courses offered by the UW-Madison in a visual and interactive way.",
   themeConfig: {
+    logo: 'https://uwcourses.com/uw-coursemap-logo.svg',
+    siteTitle: 'UW Course Map',
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
+      {
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'About',
+        link: '/about'
+      },
+      {
+        text: 'Live',
+        link: 'https://uwcourses.com'
+      },
     ],
-
+    sidebar: [
+      {
+        text: 'Getting Started',
+        items: [
+          {
+            text: 'About',
+            link: '/about'
+          },
+          {
+            text: 'Quickstart',
+            link: '/getting-started/quickstart'
+          },
+        ]
+      },
+      {
+        text: 'Concepts',
+        items: [
+          {
+            text: 'Data Model',
+            link: '/concepts/data-model'
+          },
+          {
+            text: 'Architecture',
+            link: '/concepts/architecture'
+          }
+        ]
+      },
+      {
+        text: 'Codebase',
+        items: [
+          {
+            text: 'Frontend',
+            link: '/codebase/frontend'
+          },
+          {
+            text: 'Backend',
+            link: '/codebase/backend'
+          },
+          {
+            text: 'Generation',
+            link: '/codebase/generation'
+          }
+        ]
+      },
+      {
+        text: 'Contributing',
+        link: '/contributing'
+      },
+      {
+        text: 'Team',
+        link: '/team'
+      },
+      {
+        text: 'Branding',
+        link: '/branding'
+      },
+      {
+        text: 'Advanced',
+        link: '/advanced'
+      }
+    ],
+    search: {
+      provider: 'local'
+    },
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+      {
+        icon: 'github',
+        link: 'https://github.com/twangodev/uw-coursemap'
+      }
+    ],
+    editLink: {
+      pattern: 'https://github.com/twangodev/uw-coursemap/edit/main/docs/:path',
+      text: 'Edit this page on GitHub'
+    },
+    footer: {
+      message: "UW Course Map is not affiliated with the University of Wisconsin-Madison.",
+      copyright: `&copy; ${new Date().getFullYear()} James Ding`
+    }
   },
   lastUpdated: true,
+  sitemap: {
+    hostname: 'https://docs.uwcourses.com'
+  },
 }
 
-export default defineConfig(withSidebar(vitePressOptions, vitePressSidebarOptions));
+export default defineConfig(vitePressOptions);
--- docs/index.md
+++ docs/index.md
@@ -9,7 +9,7 @@ hero:
   actions:
     - theme: brand
       text: Get Started
-      link: /1-getting-started/1-overview
+      link: /about
     - theme: alt
       text: View on GitHub
       link: https://github.com/twangodev/uw-coursemap
@@ -18,14 +18,14 @@ features:
   - title: Overview
     icon: ⚙️
     details: Quickly understand how this project works, its goals, and how to get started running it yourself.
-    link: /1-getting-started/1-overview
+    link: /about
   - title: Contributing
     icon: 🤝
     details: This project was built by students, for students. We welcome contributions from anyone who wants to help out! For more information, check out our contributing guide.
     link: /contributing
   - title: License
     icon: 📜
-    details: We're open source! We are licensed under AGPL-3.0, if you want to use our code, please check out the license.
+    details: We're open source! This project is licensed under AGPL-3.0, if you want to use our code, redistribute it, or modify it, please check out the license.
     link: https://github.com/twangodev/uw-coursemap/blob/main/LICENSE
 ---
 
