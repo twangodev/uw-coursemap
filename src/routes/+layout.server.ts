87f893747f27cf4b31f780cfb60bde3a551a59cf
ProfessorAtomicManiac <59379639+ProfessorAtomicManiac@users.noreply.github.com>
2025-04-21 10:14:05 -0500

    removed ssr

--- src/routes/+layout.server.ts
+++
@@ -1,7 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-import { getContributors } from '$lib/github';
-
-export const load: LayoutServerLoad = async () => {
-    const contributors = (await getContributors()).slice(0,5);
-    return { contributors };
-};
