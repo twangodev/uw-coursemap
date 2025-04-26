4d7badfd2b162cfb04ab1a1594e2783e69f08d35
ProfessorAtomicManiac <59379639+ProfessorAtomicManiac@users.noreply.github.com>
2025-04-26 18:10:04 -0500

    added searchBarOptions interface

--- src/lib/searchModalStore.ts
+++ src/lib/searchModalStore.ts
@@ -1,3 +1,13 @@
 import {writable} from "svelte/store";
+export interface SearchBarOptions {
+    showCourses: boolean;
+    showDepartments: boolean;
+    showInstructors: boolean;
+}
+export const searchOptions = writable<SearchBarOptions>({
+    showCourses: true,
+    showDepartments: true,
+    showInstructors: true,
+})
 
 export const searchModalOpen = writable<boolean>(false);
