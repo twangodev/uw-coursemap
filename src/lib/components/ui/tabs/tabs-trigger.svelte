d364b9b6a4552c74193fedd9bc0440e93785d3ab
piggypiggyyoinkyoink <79926591+piggypiggyyoinkyoink@users.noreply.github.com>
2025-04-27 20:10:43 +0100

    Update src/lib/components/ui/tabs/tabs-trigger.svelte
    Co-authored-by: James Ding <jamesding365@gmail.com>

--- src/lib/components/ui/tabs/tabs-trigger.svelte
+++ src/lib/components/ui/tabs/tabs-trigger.svelte
@@ -12,7 +12,7 @@
 <TabsPrimitive.Trigger
 	bind:ref
 	class={cn(
-		"ring-offset-background focus-visible:ring-ring data-[state=active]:bg-background data-[state=active]:text-foreground inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium transition-all focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-xs hover: dark:hover:bg-neutral-700 hover:bg-neutral-200 cursor-pointer",
+		"ring-offset-background focus-visible:ring-ring data-[state=active]:bg-background data-[state=active]:text-foreground inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium transition-all focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-xs hover:text-foreground cursor-pointer",
 		className
 	)}
 	{...restProps}
