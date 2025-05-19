4d94eb5626caec49866ab6dff2b52538cd49728a
James Ding <jamesding365@gmail.com>
2025-05-19 14:59:47 -0700

    Integrate tqdm logging redirection for improved progress tracking in data fetching

--- generation/webscrape.py
+++ generation/webscrape.py
@@ -86,8 +86,7 @@ async def scrape_all(urls: set[str], logger: Logger):
 
     async with aiohttp.ClientSession(timeout=timeout, connector=connector) as session:
         tasks = [get_course_blocks(session, url, logger) for url in urls]
-        with logging_redirect_tqdm():
-            results = await tqdm.gather(*tasks, desc="Departmental Course Scrape", unit="department")
+        results = await tqdm.gather(*tasks, desc="Departmental Course Scrape", unit="department")
         for full_subject, blocks in results:
             add_data(subject_to_full_subject, course_ref_to_course, full_subject, blocks, logger)
 
