d882141b4e693b6cd81ec41baa19d34818966472
James Ding <jamesding365@gmail.com>
2025-05-24 10:37:52 -0700

    Update SEO metadata with dynamic title and description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,14 +4,28 @@
     import Footer from "$lib/components/footer/footer.svelte";
     import {ModeWatcher} from "mode-watcher";
     import {Toaster} from "$lib/components/ui/sonner";
+    import { page } from '$app/state'
 	let { children } = $props();
+
+    const siteName = "UW Course Map"
+
+    let title = $derived.by(() => {
+        const subtitle = page.data?.subtitle
+        if (!subtitle) {
+            return siteName
+        }
+        return `${subtitle} | ${siteName}`
+    })
+
+    let description = $derived(page.data?.description || "Explore the courses offered by the UW-Madison in a visual and interactive way.");
+
 </script>
 
 <svelte:head>
-    <title>UW Course Map</title>
+    <title>{title}</title>
     <link rel="icon" href="/favicon.ico">
-    <meta name="description" content="Explore the courses offered by the UW-Madison in a visual and interactive way." />
-    <meta name="keywords" content="UW-Madison, course map, course explorer, university courses" />
+    <meta name="description" content={description} />
+    <meta name="keywords" content="UW-Madison, University of Wisconsin-Madison, course map, course explorer, university courses" />
     <script defer src="https://analytics.twango.dev/script.js" data-website-id="e9ccd1f1-8138-4a41-a59b-dfedd30b4744"></script>
 </svelte:head>
 
