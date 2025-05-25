8f08a832ed4c1c49f471d5e61e869f36d9b5d7d6
James Ding <jamesding365@gmail.com>
2025-05-25 16:19:45 -0700

    Add JSON-LD structured data for instructor profiles

--- src/routes/instructors/[name]/+page.ts
+++ src/routes/instructors/[name]/+page.ts
@@ -1,6 +1,7 @@
 import {env} from "$env/dynamic/public";
 import {error} from "@sveltejs/kit";
 import type {FullInstructorInformation} from "$lib/types/instructor.ts";
+import type {ProfilePage, WithContext} from "schema-dts";
 
 const PUBLIC_API_URL = env.PUBLIC_API_URL
 
@@ -12,7 +13,27 @@ export const load = async ({ params, fetch }) => {
     if (!instructorResponse.ok) throw error(instructorResponse.status, `Failed to fetch instructor: ${instructorResponse.statusText}`)
     const instructor: FullInstructorInformation = await instructorResponse.json();
 
+    const description = `${instructor.name} is a ${instructor.position || 'faculty'} at ${instructor.department || 'some department'} at the University of Wisconsin-Madison.`
+
+    const jsonLd: WithContext<ProfilePage> = {
+        '@context': 'https://schema.org',
+        '@type': 'ProfilePage',
+        mainEntity: {
+            '@type': 'Person',
+            name: instructor.name,
+            jobTitle: instructor.position || 'Faculty',
+            affiliation: {
+                '@type': 'EducationalOrganization',
+                name: 'University of Wisconsin-Madison',
+                department: instructor.department || 'Some Department'
+            },
+        }
+    }
+
     return {
-        instructor: instructor
+        subtitle: instructor.name,
+        description: description,
+        instructor: instructor,
+        jsonLd: [jsonLd]
     }
 };
