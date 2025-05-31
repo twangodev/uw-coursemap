23e10e3963b8b3c60ae07681910febc7dafd29ea
James Ding <jamesding365@gmail.com>
2025-05-31 01:14:46 -0700

    Refactor rotating tilt card transition and enhance background gradient styling

--- src/lib/components/rotating-tilt-card.svelte
+++ src/lib/components/rotating-tilt-card.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 
     import {slide, type SlideParams} from "svelte/transition";
-    import {cubicInOut} from "svelte/easing";
+    import {quadInOut} from "svelte/easing";
 
     const DISPLAY_DURATION = 5000
 
@@ -29,8 +29,8 @@
     }
 
     const slideParams: SlideParams = {
-        duration: 500,
-        easing: cubicInOut,
+        duration: 750,
+        easing: quadInOut,
         axis: "y",
     }
 
@@ -44,7 +44,7 @@
                 <div aria-hidden="true" class="bg-linear-to-b from-background to-background z-1 absolute -inset-16 via-transparent sm:-inset-32"></div>
                 <div aria-hidden="true" class="bg-linear-to-r from-background to-background z-1 absolute -inset-16 bg-white/50 via-transparent sm:-inset-32 dark:bg-transparent"></div>
                 <div aria-hidden="true" class="absolute -inset-16 bg-[linear-gradient(to_right,var(--color-border)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-border)_1px,transparent_1px)] bg-[size:24px_24px] [--color-border:var(--color-black)] sm:-inset-32 dark:[--color-border:color-mix(in_oklab,var(--color-white)_50%,transparent)]"></div>
-                <div aria-hidden="true" class="from-background/20 dark:from-background z-11 absolute inset-0 bg-gradient-to-l"></div>
+                <div aria-hidden="true" class="from-background/20 dark:from-background/60 z-11 absolute inset-0 bg-gradient-to-l"></div>
                 <div aria-hidden="true" class="z-2 absolute inset-0 size-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,transparent_40%,var(--color-background)_100%)]"></div>
                 <div aria-hidden="true" class="z-2 absolute inset-0 size-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,transparent_40%,var(--color-background)_100%)]"></div>
                 <div class="mt-8 mb-8">
