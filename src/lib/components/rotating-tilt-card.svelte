01e73eec59beb8a999af6f020d6decf102371492
James Ding <jamesding365@gmail.com>
2025-05-31 01:07:23 -0700

    Refactor rotating tilt card layout and enhance image display styling

--- src/lib/components/rotating-tilt-card.svelte
+++ src/lib/components/rotating-tilt-card.svelte
@@ -38,33 +38,35 @@
 </script>
 
 <div class="overflow-hidden -mt-16 mx-auto max-w-7xl lg:pr-44">
-    <div class="perspective-distant -mr-16 pl-16 lg:-mr-56 lg:pl-56">
+    <div class="perspective-distant -mr-16 lg:-mr-56 lg:pl-32">
         <div class="[transform:rotateX(20deg);]">
-            <div class="lg:h-176 relative skew-x-[.32rad]">
+            <div class="lg:h-176 relative skew-x-[.25rad]">
                 <div aria-hidden="true" class="bg-linear-to-b from-background to-background z-1 absolute -inset-16 via-transparent sm:-inset-32"></div>
                 <div aria-hidden="true" class="bg-linear-to-r from-background to-background z-1 absolute -inset-16 bg-white/50 via-transparent sm:-inset-32 dark:bg-transparent"></div>
-                <div aria-hidden="true" class="absolute -inset-16 bg-[linear-gradient(to_right,var(--color-border)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-border)_1px,transparent_1px)] bg-[size:24px_24px] [--color-border:var(--color-zinc-600)] sm:-inset-32 dark:[--color-border:color-mix(in_oklab,var(--color-white)_20%,transparent)]"></div>
-                <div aria-hidden="true" class="from-background z-11 absolute inset-0 bg-gradient-to-l"></div>
+                <div aria-hidden="true" class="absolute -inset-16 bg-[linear-gradient(to_right,var(--color-border)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-border)_1px,transparent_1px)] bg-[size:24px_24px] [--color-border:var(--color-black)] sm:-inset-32 dark:[--color-border:color-mix(in_oklab,var(--color-white)_50%,transparent)]"></div>
+                <div aria-hidden="true" class="from-background/20 dark:from-background z-11 absolute inset-0 bg-gradient-to-l"></div>
                 <div aria-hidden="true" class="z-2 absolute inset-0 size-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,transparent_40%,var(--color-background)_100%)]"></div>
                 <div aria-hidden="true" class="z-2 absolute inset-0 size-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,transparent_40%,var(--color-background)_100%)]"></div>
-                {#key cardImages[i]}
-                    <div class="z-1 relative mx-auto" transition:slide={slideParams}>
-                        <img
-                                class="rounded-(--radius) z-1 relative dark:hidden"
-                                src={cardImages[i]}
-                                alt="COMPSCI 300 Preview"
-                                width=3840
-                                height=2160
-                        />
-                        <img
-                                class="rounded-(--radius) z-1 relative hidden dark:block"
-                                src={toDarkVariant(cardImages[i])}
-                                alt="COMPSCI 300 Preview"
-                                width=3840
-                                height=2160
-                        />
-                    </div>
-                {/key}
+                <div class="mt-8 mb-8">
+                    {#key cardImages[i]}
+                        <div class="rounded-(--radius) z-1 relative border-2 mx-auto" transition:slide={slideParams}>
+                            <img
+                                    class="rounded-(--radius) z-1 relative dark:hidden"
+                                    src={cardImages[i]}
+                                    alt="COMPSCI 300 Preview"
+                                    width=3840
+                                    height=2160
+                            />
+                            <img
+                                    class="rounded-(--radius) z-1 relative hidden dark:block"
+                                    src={toDarkVariant(cardImages[i])}
+                                    alt="COMPSCI 300 Preview"
+                                    width=3840
+                                    height=2160
+                            />
+                        </div>
+                    {/key}
+                </div>
             </div>
         </div>
     </div>
