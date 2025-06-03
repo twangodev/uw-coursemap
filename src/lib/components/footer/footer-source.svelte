e923c5a71dd468d6ac766de51c7e05956e93153c
James Ding <jamesding365@gmail.com>
2025-06-02 19:45:01 -0700

    Update footer links to point to GitHub profiles

--- src/lib/components/footer/footer-source.svelte
+++ src/lib/components/footer/footer-source.svelte
@@ -3,8 +3,8 @@
 </script>
 
 Made possible by
-<FooterLink href="https://twango.dev">James Ding</FooterLink>,
-<FooterLink href="https://">Charles Ding</FooterLink>, and
+<FooterLink href="https://github.com/twangodev">James Ding</FooterLink>,
+<FooterLink href="https://github.com/ProfessorAtomicManiac">Charles Ding</FooterLink>, and
 <FooterLink href="https://docs.uwcourses.com/team">contributors</FooterLink>.
 
 View source on{' '}
